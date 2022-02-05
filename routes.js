2ad5e61b7ef7bd786b6c4f65564c654a4d70a85b
Al Willmott <60848405+beanolabub@users.noreply.github.com>
2022-02-05 23:52:28 +0000

    Add files via upload
    Update to get new tasks added to the database.

--- routes.js
+++ routes.js
@@ -1,16 +1,44 @@
 const express = require('express');
 const mysql = require('mysql');
 const router = express.Router();
+const conn = getConnection();
+const pass = "password"
 
-//console.log("express", express);
-//console.log("mysql", mysql);
-//console.log("router", router);
+function getConnection() {
+    return mysql.createConnection({
+        host: "localhost",
+        port: "3306",
+        user: "root",
+        password: "",
+        database : "todo"
+    })
+}
 
 router.get('/', (req, res) => {
     res.render('index');
 });
 
-//router.post('/add_todo')
+router.get('/get_todos', (req, res) =>{
+    const queryString = "SELECT * FROM todos WHERE complete = '0'"
+    conn.query(queryString, (err, rows, fields) =>{
+        if (err) {
+            console.log("failed to query @ /get_todo: " + err);
+        }
+        console.log("Getting data from database at @ /get_todos");
+        res.json(rows);
+    })
+});
+
+router.post('/add_todo', (req, res) =>{
+    const todo = req.body.add_todo_input;
+    const queryString = "INSERT INTO todos (todo) VALUES (?)";
+    conn.query(queryString, [todo], (err, rows, fields) => {
+        if (err){
+            console.log("failed to insert @ /add_todo: " + todo + " " + err);
+        }
+        console.log("@ /add_todo : " + todo + " added");
+        res.redirect('/');
+    })
+})
+
 module.exports = router;
-//console.log("routerGet", router);
-console.log("module", module.exports);
