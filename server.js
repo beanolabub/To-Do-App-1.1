43c5ebfcf1dd44f63168e8675cbb2942d1e30b77
Al Willmott <60848405+beanolabub@users.noreply.github.com>
2022-03-10 20:57:42 +0000

    added favicon reference

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const routes = require('./routes');
 const app = express();
 const bodyParser = require('body-parser');
 
+app.use(express.static('images'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','ejs');
+
 
 const middlewares = [
     layout(),
@@ -20,5 +22,8 @@ app.listen(3535, () => {
     console.log('listening on port 3535');
 });
 
+
+
 app.use('/js', express.static(path.join(__dirname, 'js')));
 app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
