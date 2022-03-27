50fce69faa58a7cacaf53c6dc9cf95ef625f8dfa
wheeliemanuk <wheeliemanuk@gmail.com>
2022-03-27 13:28:25 +0100

    js update

--- js/javascript.js
+++ js/javascript.js
@@ -1,15 +1,15 @@
 get_todos()
 get_complete()
 
-function get_todos() {
+function get_todos(order) {
    var request = new XMLHttpRequest();
     var requestURL = '/get_todos';
     request.open('GET', requestURL);
     request.responseType = 'json';
     request.send();
     request.onload = function() {
-        var todos = request.response;       
-        console.log(todos);
+        var todos = request.response;
+        if (order === 1) todos.sort((a,b) => (a.TODO > b.TODO) ? 1 : ((b.TODO > a.TODO) ? -1 : 0));
         printTodos(todos);
     }
 }
@@ -29,32 +29,21 @@ function get_complete() {
 
 function printTodos(todos) {
     let table = document.getElementById('todo_table');
+    let row = '';
     for (var i in todos ) {
         // data
         const todo_id = todos[i].todo_id;
         const todo = todos[i].TODO;
         const todo_createdDate = formatDates(todos[i].DATE_CREATED);
-        // create elements\
-        var row = document.createElement('tr');
-        var todo_cell = document.createElement('td');
-        var todo_cell1 = document.createElement('td');
-        var todo_cell2 = document.createElement('td');
-        var todo_cell3 = document.createElement('td');
-        var todo_trashButton = document.createElement('button');
-        // delete icon
-        const todo_trash = '<i class="fa fa-trash" onclick="deleteTodo(' + todo_id +')"></i>';
-        const todo_button = '<i class="fa fa-check" onclick="completeTodo(' + todo_id +')"></i>';
-        // innerHTML
-        todo_cell.innerHTML = todo;        
-        todo_cell1.innerHTML = todo_button;
-        todo_cell2.innerHTML = todo_createdDate; 
-        todo_cell3.innerHTML = todo_trash; 
-        // append
-        row.append(todo_cell);
-        row.append(todo_cell2);
-        row.append(todo_cell1);
-        row.append(todo_cell3);
-        table.append(row);
+        // create row
+        row += `
+        <tr>
+            <td>${todo}</td>
+            <td>${todo_createdDate}</td>
+            <td><i class="fa fa-trash" onclick="deleteTodo(${todo_id})"></i></td>
+            <td><i class="fa fa-check" onclick="completeTodo(${todo_id})"></i></td>
+        </tr>`;
+        table.innerHTML = (row);
 
     }
 }
@@ -102,6 +91,7 @@ function completeTodo(todo_id) {
 
 function deleteTodo(todo_id) {
     var form = document.getElementById("delete_todo_form");
+   
     form.action = form.action + todo_id;
     form.submit();
 }
