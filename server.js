3efaba381749fe16a377ff6b4b856b3d673207e2
Al Willmott <60848405+beanolabub@users.noreply.github.com>
2022-04-21 19:35:00 +0100

    BillyBob
    Authentication Auth0

--- server.js
+++ server.js
@@ -4,23 +4,36 @@ const layout = require('express-layout');
 const routes = require('./routes');
 const app = express();
 const bodyParser = require('body-parser');
-
-app.use(express.static('images'));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine','ejs');
-
+const { auth } = require('express-openid-connect');
+require('dotenv').config();
 const middlewares = [
     layout(),
     express.static(path.join(__dirname, 'public')), 
     bodyParser.urlencoded({extended : false})
 ]
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASEURL,
+    clientID: process.env.CLIENTID,
+    issuerBaseURL: process.env.ISSUER,
+  };
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine','ejs');
+
+app.use(express.static('images'));
 app.use(middlewares);
-
-app.use('/', routes);
-app.listen(3535, () => {
-    console.log('listening on port 3535');
-});
-
 app.use('/js', express.static(path.join(__dirname, 'js')));
 app.use('/css', express.static(path.join(__dirname, 'css')));
 app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use(auth(config));
+app.use('/', routes);
+
+app.listen(3535, () => {
+    console.log('listening on port 3535');
+});
